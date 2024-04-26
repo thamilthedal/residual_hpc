010025d0a9c54e90912401ef3df321b52d424a7b
thamilthedal <57757731+thamilthedal@users.noreply.github.com>
2024-04-26 11:25:20 +0530

    updated version

--- residue.py
+++ residue.py
@@ -1,11 +1,8 @@
 import monitor.monitor as mm
-import monitor.helper as mh
 import sys
 
 def run(file_name):
-    while(True):
-        [residual, legend, last_id]= mh.get_residue(file_name)
-        mm.monitor_residue(file_name, residual, legend)
+    mm.start_monitor()
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
