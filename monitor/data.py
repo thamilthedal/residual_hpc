73a4024db3088392d06d0e8be10afabcd9a57c7f
thamilthedal <57757731+thamilthedal@users.noreply.github.com>
2024-04-26 11:24:50 +0530

    Update data.py

--- monitor/data.py
+++ monitor/data.py
@@ -1,7 +1,18 @@
+import sys
+from lib.plot_utils import set_plot
+from lib.plot_settings import PLOT_STYLE_RES, RES_MARKERS
+
 # SSH Credentials
 HOST_NAME = 'xyz.com'
 USER = 'userid'
 PWD = '********'
 
 # Plot Details
-TITLE = ['Residue Monitor', 'Number of Iterations', 'Normalised Residuals']
+file_name = sys.argv[1].split('/')[-1].split('.')[0]
+Title = [f"${file_name}$", "$Number\ of\ Iterations$", "$Normalized\ Residuals$"]
+X = [0, 1, 0.1, "linear"]
+Y = [0.1, 1000, 10, "log"]
+
+fig, ax = set_plot(Title, X, Y, (8, 6), style=PLOT_STYLE_RES)
+
+WORDS = ['flow', '#', 'warning', 'details', 'Time', 'survey.', 'anonymous', 'use']
