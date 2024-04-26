0de41cdc566eeac2ed4a96185eef3868bde3ecee
thamilthedal <57757731+thamilthedal@users.noreply.github.com>
2024-04-26 11:26:45 +0530

    Add files via upload

---
+++ lib/plot_settings.py
@@ -0,0 +1,83 @@
+PLOT_STYLE: dict[str, list[bool] | int | str | float] = {
+    "font": 'Times New Roman',
+    "title_size": 22,
+    "label_size": 22,
+    "internal_fontsize":18,
+    "label_weight": 'bold',
+    "line_width": 0.75,
+    "border": [True, True, True, True],
+}
+
+PLOT_STYLE_2: dict[str, list[bool] | int | str | float] = {
+    "font": 'Times New Roman',
+    "title_size": 22,
+    "label_size": 22,
+    "internal_fontsize":18,
+    "label_weight": 'bold',
+    "line_width": 0.75,
+    "border": [False, False, True, True],
+}
+
+PLOT_STYLE_RES: dict[str, list[bool] | int | str | float] = {
+    "font": 'Times New Roman',
+    "title_size": 20,
+    "label_size": 16,
+    "internal_fontsize":16,
+    "label_weight": 'bold',
+    "line_width": 0.75,
+    "border": [False, False, True, True],
+}
+MULTI_STYLE: dict[str, list[bool] | int | str | float] = {
+    "font": 'Times New Roman',
+    "title_size": 22,
+    "label_size": 22,
+    "internal_fontsize":18,
+    "label_weight": 'bold',
+    "line_width": 0.75,
+    "border": [True, True, True, True],
+}
+
+MARKERS = [
+        's',
+        'ko--',
+        'rD-.',
+        'g^:',
+        'bd--',
+        'm.-',
+        'c+:',
+        'v-.'
+        ]
+RES_MARKERS = [
+        'k--',
+        'r-.',
+        'g:',
+        'b--',
+        'm-',
+        'c:',
+        '-'
+        ]
+
+BLACK_MARKERS = [
+        'ks',
+        'ko--',
+        'kD-.',
+        'k^:',
+        'kd--',
+        'k.-',
+        'k+:',
+        'kv-.'
+        ]
+
+SPLIT_MARKERS = [
+        'k^-',
+        'k^--',
+        'rs-',
+        'rs--',
+        'go-',
+        'go--',
+        'bd-',
+        'bd--',
+        'm+-',
+        'm+--'
+        ]
+
