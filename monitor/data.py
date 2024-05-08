5953e9afde6669f4c671dd2243bee170c34ac069
thamilthedal <57757731+thamilthedal@users.noreply.github.com>
2024-05-08 05:26:10 -0700

    Update data.py

--- monitor/data.py
+++ monitor/data.py
@@ -16,6 +16,6 @@ Title = [f"${file_name}$", "$Number\ of\ Iterations$", "$Normalized\ Residuals$"
 X = [0, 1, 0.1, "linear"]
 Y = [0.1, 1000, 10, "log"]
 
-fig, ax = set_plot(Title, X, Y, (8, 6), style=PLOT_STYLE_RES)
+fig, ax = set_plot(Title, X, Y, (8, 6), PLOT_STYLE_RES)
 
 WORDS = ['flow', '#', 'warning', 'details', 'Time', 'survey.', 'anonymous', 'use']
