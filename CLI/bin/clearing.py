805d1bcf27f21336d9964beb8d0ddeb139ddb455
thamilthedal <thamilthedal@gmail.com>
2025-10-06 14:23:49 +0530

    directory cleaning

---
+++ CLI/bin/clearing.py
@@ -0,0 +1,11 @@
+import shutil
+
+def _clear_pyc():
+    folder_paths = [
+                    "./CLI/bin/__pycache__",
+                    "./CLI/lib/__pycache__",
+                    "./CLI/__pycache__"
+            ]
+    for folder in folder_paths:
+        shutil.rmtree(folder)
+
--- CLI/main.py
+++ CLI/main.py
@@ -3,19 +3,19 @@ import click
 
 @click.group()
 def monitor():
-    from CLI.bin.commands import _jobs
-    _jobs()
+    from CLI.bin.cluster import print_jobs
+    print_jobs()
 
 @monitor.command()
 def file():
-    from CLI.bin.commands import _clear_pyc
+    from CLI.bin.clearing import _clear_pyc
     from CLI.bin.multifile import multi_file_monitor
     multi_file_monitor()
     _clear_pyc()
 
 @monitor.command()
 def residue():
-    from CLI.bin.commands import _clear_pyc
+    from CLI.bin.clearing import _clear_pyc
     from CLI.bin.multiresidue import multi_residue_monitor
     multi_residue_monitor()
     _clear_pyc()
