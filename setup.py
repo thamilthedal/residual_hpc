d3c3c09768ccb50bdacca15d1dbd34e2d73f05b6
thamilthedal <thamilthedal@gmail.com>
2026-06-22 17:38:28 +0530

    color palette extended to 10 colors, settings moved out of data, first_login and data merged

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="monitor-cli",
-    version="2.0",
+    version="2.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
