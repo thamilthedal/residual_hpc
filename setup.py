c4ccf36874a74d4ea46cae89f4da3911b5a929fa
thamilthedal <thamilthedal@gmail.com>
2026-06-22 14:45:22 +0530

    automated legends and moved start_ID finding to only once

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="monitor-cli",
-    version="1.0",
+    version="2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
